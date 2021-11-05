f2ff9ecf925823541ecc39a8b358c6ab47246016
Ubuntu <ubuntu@ip-172-31-35-40.ap-northeast-2.compute.internal>
2021-11-05 04:40:30 +0000

    bettingCheck

---
+++ routes/home/post_bettingCheck.js
@@ -0,0 +1,51 @@
+/**
+ * @file 
+ * 개발 샘플
+ *
+ * GET /sample/test2
+ */
+
+const exValidator = require("express-validator");
+var async = require('async');
+/*
+Express Validator 6.2
+https://express-validator.github.io/docs/schema-validation.html
+https://auth0.com/blog/express-validator-tutorial/
+*/
+var db = require("../../module/mongodbWrapper");
+var rank = require("../../module/rankingPost");
+var power = require("../../module/funcPoint");
+var dbCache = require("../../module/dbCache");
+var ObjectId = require('mongodb').ObjectId;
+
+module.exports = [
+    '/bettingCheck', // URI : /test2 (최종은 /<route module>/<page module> : /sameple/test2)
+    [], // 권한 : null은 권한 미체크, [] array 형as태로 권한 지정
+    [
+        // Validation 처리 ------------------------------------
+        exValidator.body('cId', 'REQ').not().isEmpty().trim(),
+        //      exValidator.header('user-agent', 'NOT FOUND').not().isEmpty().isLength({min: 4,max: 24})
+        //        .matches(/^MY/)  // MY로 시작되는 문자열
+        //        .custom((value, {req,location,path}) => { return value.startsWith('MY'); }) // MY로 시작되는 문자열(위의 matches()랑 동일 구현)
+        // 파라미터 sanitize 처리 ------------------------------7
+        //      sanitize* function들은 모두 validation 과 통합되었음
+    ],
+    async function (req, res, next) {
+        // server-side validation 처리
+        const validationErrors = exValidator.validationResult(req);
+        if (!validationErrors.isEmpty()) {
+            return res.sendStatus(404);
+        }
+        var contentId = req.body.cId;
+        var userId = req.user ? req.user._id.toHexString() : null;
+        var resultCheck = await power.bettingCheck(userId, contentId)
+
+        //관리자가 댓글 작성자 인끼업 운영자중 선택한 경우
+        return res.json({
+            result: resultCheck ? "ok" : "error",
+            codemsg: "betting-check-post",
+            dispaymsg: "",
+            data: resultCheck
+        });
+    }
+];
