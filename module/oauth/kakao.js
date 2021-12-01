e3d064a9d84824f76fe67a3730a8fb19b89ad0b9
jonghyun <jonghyun7147@gmail.com>
2021-12-01 15:44:17 +0900

    kakaoPhoto check

--- module/oauth/kakao.js
+++ module/oauth/kakao.js
@@ -21,10 +21,10 @@ async function signup(deviceId, userId, token, nickname, req) {
       }
     })
 
+    console.log('kakaoDATA', response)
     if (response && response.status === 200 && response.data && response.data.id) {
       // 사용자 정보를 필요에 따라서 저장함
       // 상시 업데이트와 초기에만 업데이트하는 것을 구분해야 함(setOnInsert)
-      console.log('kakaoDATA', response.data)
       const userInfo = await db.update('user', $where, {
         $set: {
           "auth": {
